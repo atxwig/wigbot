910327bd6948829c4b03c2c784f4bca9087ae684
Colette Montminy <cmontminy@utexas.edu>
2020-07-15 23:33:46 -0500

    fix invite command

--- wig.py
+++ wig.py
@@ -9,8 +9,8 @@ import traceback
 
 
 # bot description
-command_prefix = '!w '
-description = "beepo for the boys"
+command_prefix = '-'
+description = "wigwigwig"
 bot = commands.Bot(command_prefix=command_prefix, description=description,
                    case_insensitive=True)
 
@@ -18,8 +18,15 @@ bot = commands.Bot(command_prefix=command_prefix, description=description,
 # globals
 guild_id = 550143114417930250  # TODO: find a way to not hardcode
 cached_invite_list = {}
-default_channel = 0
+default_channel_id = None
 
+# token = 0
+# f = open("secrets.txt", "r") # fetch token from secrets file
+# lines = f.readlines()
+# for line in lines:
+#     if "TOKEN" in line:
+#         line_list = line.split("=")
+#         token = line_list[1]
 
 # start up
 @bot.event
@@ -34,20 +41,26 @@ async def on_ready():
 # set channel command
 @bot.command()
 async def setchannel(ctx):
-    global default_channel
-    default_channel = ctx.message.channel
-    await ctx.send(f"#{default_channel} has been set as the default channel!")
+    global default_channel_id
+    default_channel_id = ctx.message.channel.id
+    await ctx.send(f"{bot.get_channel(default_channel_id).mention} has been set as the default channel!")
 
+# get channel command
+@bot.command()
+async def getchannel(ctx):
+    global default_channel_id
+    await ctx.send(f"The default channel is {bot.get_channel(default_channel_id).mention}.")
 
 # member join
 @bot.event
 async def on_member_join(member):
+    global default_channel_id
     invite_id_list = await member.guild.invites()  # fetch current invite uses
 
     curr_invite_list = {}
     for invite in invite_id_list:
         curr_invite_list[invite.id] = invite.uses
-    message = f"**{member.display_name}** has joined!"
+    message = f"**{member.name}** has joined!"
 
     for invite in invite_id_list:
         if curr_invite_list[invite.id] != cached_invite_list[invite.id]:
@@ -55,7 +68,7 @@ async def on_member_join(member):
             break
 
     await cache_invites()
-    await bot.get_channel(default_channel.id).send(message)
+    await bot.get_channel(default_channel_id).send(message)
 
 
 # gets current invites
@@ -65,7 +78,6 @@ async def cache_invites():
     for invite in invite_id_list:
         cached_invite_list[invite.id] = invite.uses
         print(f"added invite {invite.id}")
-
 
 # test command
 @bot.command()
@@ -79,12 +91,12 @@ async def helpme(ctx):
     embed = discord.Embed(title="TWIGGY COMMANDS",
                           description=f"Command prefix = {command_prefix}")
     embed.add_field(
-        name=f"{command_prefix} test", value="Sends wigwigwig", inline=False)
+        name=f"{command_prefix}test", value="Sends wigwigwig", inline=False)
 
-    embed.add_field(name=f"{command_prefix} setchannel",
+    embed.add_field(name=f"{command_prefix}setchannel",
                     value="Sets current channel to default channel. (Right now just picks what channel to send the invite messages to.", inline=False)
 
-    embed.add_field(name=f"{command_prefix} hug [@user]",
+    embed.add_field(name=f"{command_prefix}hug [@user]",
                     value="Sends a cute hug to whoever you at uwu", inline=False)
 
     await ctx.send(embed=embed)
@@ -94,9 +106,7 @@ async def helpme(ctx):
 @bot.command()
 async def hug(ctx, member_id):
     member_id = member_id[3:-1]
-    print(member_id)
     member = ctx.message.guild.get_member(int(member_id))
-    print(member.nick)
     if member:
         await ctx.send(f"{ctx.message.author.mention} is omega cute and hugged {member.mention}!")
     else:
@@ -104,3 +114,4 @@ async def hug(ctx, member_id):
 
 
 bot.run(os.environ.get('BOT_TOKEN'))
+# bot.run(token)
